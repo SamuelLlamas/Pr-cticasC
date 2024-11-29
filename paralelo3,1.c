610d7de4cd8f0d816df532bb4618b74b5b88561d
SamuelLlamas <samuelllamasmartinez@gmail.com>
2024-11-29 20:37:23 +0100

    Add files via upload

---
+++ paralelo3,1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int nthreads, tid, media;
+    int n;
+    printf("Introduce n: ");
+    scanf("%d", &n);
+    int sum = 0;
+    int *v = (int*)malloc(sizeof(int)*n);
+    double t1 = omp_get_wtime();
+
+    #pragma omp parallel private (tid)
+      
+    {      
+        srand(tid);
+        #pragma omp for 
+        for(int i=0; i<n; i++){
+            *(v+i) = rand()%100+1;
+            printf("%d\n", *(v+i));
+        }
+        #pragma omp for 
+        for(int i = 0; i<n; i++){
+            #pragma omp atomic
+            sum+= v[i];
+            }
+        #pragma omp single
+        media = sum/n;                   
+        }
+    
+    double t2 = omp_get_wtime();
+    printf("La media del vector es: %d\n", media);
+    printf("Tiempo %lf", t2-t1);
+    
+    free(v);
+    return 0;
+}
