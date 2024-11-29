61d145cc43cccfa50595397966bfdf1b8d1e0390
SamuelLlamas <samuelllamasmartinez@gmail.com>
2024-11-29 20:17:12 +0100

    Add files via upload

---
+++ divisores.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void inicializador( int primos[10]);
+int calculadivisores( int numero);
+int esprimo(int primo);
+int main(){
+    int pr;int num;
+    calculaprimos(num);
+    
+    printf("Dime número: ");
+    scanf("%d", &num);
+    num = calculadivisores(num);
+    printf("La cantidad de divisores es : %d");
+    return = 0 ; 
+  }
+    
+    
+    
+void inicializador(int primos[10]){
+    int i; 
+    } 
+    
+    
+    
+int calculadivisores( int numero){
+    int i;int x=0;
+    for ( i=1; i<numero; i++){
+        if ( num%i == 0){
+            printf("Un divisor es: %d"; numero)
+            x = x+1;
+            }
+          }
+        return(x);
+        }
+        
+      
+        
